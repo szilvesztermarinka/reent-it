1569b9077d700d11e17968874a16b7a18194056a
JustCheddar <hagyoc@gmail.com>
2024-12-12 11:33:40 +0100

    404 update

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header"
-import kep from "../assets/404kep.png"
+import kep from "../assets/404.png"
+import { Link } from "react-router-dom";
 
 const Notfound = () => {
     return (
@@ -9,7 +10,10 @@ const Notfound = () => {
                 <div className="flex justify-center items-center h-screen">
 
 
-                    <div className="lg:p-24 md:p-52 sm:20 p-8 w-full lg:w-1/2 xl:w-3/4 h-screen flex flex-col justify-center items-center -10 text-blue-500 text-4xl text-weight-bold playwrite">Sajnos ez az oldal nem található!</div>
+                    <div className="lg:p-24 md:p-52 sm:20 p-8 w-full lg:w-1/2 xl:w-3/4 h-screen flex flex-col justify-center items-left -10 text-black-500 text-4xl text-weight-bold playwrite">Ez az oldal elérhetetlen – de az álomotthonod nem az!
+                        <p className="text-xl playwrite pt-4 text-gray-500 pb-2 ">Úgy tűnik, rossz helyen jársz, de ne aggódj, a tökéletes ingatlan még vár rád. Böngéssz hirdetéseink között, és találj rá!</p>
+                        <Link to={"/"} className="playwrite  text-2xl text-blue-500 ">Vissza a főoldalra!</Link>
+                    </div>
 
                     <div className="lg:p-24 md:p-52 sm:20 p-8 w-full lg:w-1/2 xl:w-3/4 h-screen flex flex-col justify-center items-center -10">
                         <img src={kep} alt="" />
