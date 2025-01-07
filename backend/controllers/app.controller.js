2b888b05e0ca9f98a6c6986773744de687425c39
Szilveszter Marinka <szilvesztermarinka4@gmail.com>
2025-01-07 10:29:35 +0100

    Ad filter

--- backend/controllers/app.controller.js
+++ backend/controllers/app.controller.js
@@ -1,41 +1,46 @@
-import { CookingPot } from "lucide-react";
 import { prisma } from "../config/prismaclient.js";
-import supabase from "../config/supabaseclient.js";
+
+const OPERATORS = {
+  lte: "lte",
+  gte: "gte",
+  contains: "contains",
+  startsWith: "startsWith",
+  endsWith: "endsWith",
+};
 
 export const getAllAd = async (req, res) => {
-    try {
-        const posts = await prisma.post.findMany();
-        
-        res.status(200).json({success: true, posts})
-    } catch (error) {
-        console.log(error)
+  try {
+    const query = req.query;
+    const filters = {};
+
+    for (const [key, value] of Object.entries(query)) {
+      const [field, operator] = key.split("_");
+      if (operator && OPERATORS[operator]) {
+        filters[field] = {
+            ...(filters[field] || {}),
+            [OPERATORS[operator]]: isNaN(value) ? value : parseFloat(value),
+        };
+    } else {
+        filters[key] = isNaN(value) ? value : parseFloat(value);
+      }
     }
-}
+
+    console.log(filters)
+
+    const ads = await prisma.post.findMany({
+      where: filters,
+    });
+
+    if (!ads || ads.length === 0) {
+        return res.status(404).json({ success: false, message: "No ads found" });
+      }
 
 
-export const addPost = async (req, res) => {
-    try {
-        const {property, type, price, description, deposit, bedroom, livingroom, images, balcony, ownerId} = req.body;
-
-        console.log(req.body)
-
-        const post = await prisma.post.create({
-            data: {
-                property,
-                type, 
-                price,
-                description,
-                deposit,
-                bedroom,
-                livingroom,
-                images,
-                balcony,
-                ownerId
-            }
-        })
-
-        res.status(201).json({success: true, post})
-    } catch (error) {
-        console.log(error)
-    }
-}
+    res.status(200).json({
+        success: true,
+        ads
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
--- backend/routes/app.route.js
+++ backend/routes/app.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { addPost, getAllAd } from "../controllers/app.controller.js";
+import {getAllAd } from "../controllers/app.controller.js";
 
 const router = express.Router();
 
 router.get("/all-ad", getAllAd)
-router.post("/add-post", addPost)
+//router.post("/add-post", addPost)
 
 export default router;
