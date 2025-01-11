c744fd5ad899ae2a9acb6c65a9687acf039a6931
JustCheddar <hagyoc@gmail.com>
2025-01-11 17:59:01 +0100

    filters

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,7 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { IconHome,IconBuildingSkyscraper,IconBed,IconBuildingCommunity} from '@tabler/icons-react';
+
+
 
 function Sidebar({ onFiltersChange }) {
   const [filters, setFilters] = useState({});
+
+  const handleButtonClick = (name, value) => {
+    const newFilters = { ...filters };
+
+    // Ensure one filter is always selected
+    if (newFilters[name] !== value) {
+      newFilters[name] = value;
+    }
+
+    setFilters(newFilters);
+    onFiltersChange(newFilters);
+  };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -18,17 +33,59 @@ function Sidebar({ onFiltersChange }) {
   };
 
   return (
-    <div className="w-1/3 bg-white">
-      <h1>Ingatlan Szűrő</h1>
-      <div>
-        <label>
-          Típus:
-          <select name="type" onChange={handleInputChange}>
+    <div className="w-1/5 bg-white">
+   
+      <div className="px-16 py-6 pr-6 ">
+        
+      
+      <p className="font-bold text-base py-4">Ingatlan típus</p>
+       
+            
+          <div className="grid grid-cols-2 gap-x-2 gap-y-4 justify-items-center ">
+            <div className="w-full">
+          <button
+            className={`flex flex-col items-center px-4 py-6 rounded-lg w-full ${filters.type === "" ? "bg-main-lila text-white" : "bg-gray-200"}`}
+            onClick={() => handleButtonClick("type", "")}
+          >
+            <IconBuildingCommunity />
+            Mind
+          </button>
+          </div>
+
+          <div className="w-full">
+          <button
+            className={`flex flex-col items-center px-4 py-6 rounded-lg w-full ${filters.type === "flat" ? "bg-main-lila text-white" : "bg-gray-200"}`}
+            onClick={() => handleButtonClick("type", "flat")}
+          >
+            <IconBuildingSkyscraper/>
+            Lakás
+          </button>
+          </div>
+          <div className="w-full">
+          <button
+            className={`flex flex-col items-center px-4 py-6 rounded-lg w-full ${filters.type === "house" ? "bg-main-lila text-white" : "bg-gray-200"}`}
+            onClick={() => handleButtonClick("type", "house")}
+          ><IconHome />
+            Ház
+          </button>
+          </div>
+          <div className="w-full">
+          <button
+            className={`flex flex-col items-center px-4 py-6 rounded-lg w-full ${filters.type === "room" ? "bg-main-lila text-white" : "bg-gray-200"}`}
+            onClick={() => handleButtonClick("type", "room")}
+          >
+              <IconBed/>
+            Szoba
+          </button>
+          </div>
+          
+
+          {/* <select name="type" onChange={handleInputChange}>
             <option value="">Mind</option>
             <option value="flat">Társasház</option>
             <option value="house">Családi ház</option>
-          </select>
-        </label>
+          </select> */}
+        </div>
       </div>
       <div>
         <label>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors:{'main-lila':'#724EE9',}
+        },
     },
     plugins: [],
 };
