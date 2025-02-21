4be8f294b009e3fd6414bc6d5691b45ab4e7e1b3
szilvesztermarinka <marinkaszilveszterrichard@gmail.com>
2025-02-21 06:51:44 +0100

    feat: enhance PostPage layout with improved image display and property details

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -28,32 +28,69 @@ const PostPage = () => {
 
       {ad ? (
         <div className="flex flex-1 px-16 mx-0 my-auto gap-6 mt-10">
-        <div className="w-1/2 max-w-2xl">
-          {/* Nagy kép */}
-          {ad.images && ad.images.length > 0 && (
-            <img
-              src={ad.images[0]}
-              alt="image"
-              className="rounded-xl w-full"
-            />
-          )}
-      
-          {/* Kisképek alatta - maximum 3 darab */}
-          {ad.images && ad.images.length > 1 && (
-            <div className="flex flex-row gap-3 mt-4 w-full">
-              {ad.images.slice(1, 4).map((image, index) => (
-                <div key={index} className="w-2/6 aspect-square w-full">
-                  <img
-                    src={image}
-                    alt="image"
-                    className="rounded-xl w-full h-full object-cover"
-                  />
-                </div>
-              ))}
+          <div className="w-1/2 max-w-2xl">
+            {/* Nagy kép */}
+            {ad.images && ad.images.length > 0 && (
+              <img
+                src={ad.images[0]}
+                alt="thumbnail"
+                className="rounded-xl w-full aspect-square"
+              />
+            )}
+
+            {/* Kisképek alatta - maximum 3 darab */}
+            {ad.images && ad.images.length > 1 && (
+              <div className="flex flex-row gap-3 mt-4 w-full">
+                {ad.images.slice(1, 4).map((image, index) => (
+                  <div key={index} className="w-2/6 aspect-square w-full">
+                    <img
+                      src={image}
+                      alt="asd"
+                      className="rounded-xl w-full h-full object-cover"
+                    />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+
+          <div className="w-1/2 max-w-2xl">
+
+            {/* Ár */}
+            <div>
+              <h1 className="text-2xl font-bold">{ad.price.toLocaleString("hu-HU")} Ft</h1>
+              <p className="text-gray-500">{ad.type = "Rent" && "Havonta"}</p>
             </div>
-          )}
+            {/* Áttekintés */}
+            <div>
+              <p className="font-bold text-lg">Áttekintés</p>
+              <div className="grid grid-cols-3 border border-gray-300 rounded-lg divide-x divide-y divide-gray-300 mt-2">
+                <div className="flex items-center gap-2 p-4">
+                  <span className="text-lg">🛏️</span>
+                  <p>3 szoba</p>
+                </div>
+                <div className="flex items-center gap-2 p-4">
+                  <span className="text-lg">🛁</span>
+                  <p>1 fürdőszoba</p>
+                </div>
+                <div className="flex items-center gap-2 p-4">
+                  <span className="text-lg">📏</span>
+                  <p>86 m²</p>
+                </div>
+                <div className="flex items-center gap-2 p-4">
+                  <span className="text-lg">🛋️</span>
+                  <p>Bútorozott</p>
+                </div>
+                <div className="p-4"></div> {/* Üres cella */}
+                <div className="p-4"></div> {/* Üres cella */}
+              </div>
+            </div>
+
+
+
+
+          </div>
         </div>
-      </div>
       ) : (
         <p>Loading...</p>
       )}
